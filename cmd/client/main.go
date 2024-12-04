3156ba410e7d8373d803e3a7316980e8dc6c2ba3
Liudmila <l.litvinski@gmail.com>
2024-12-04 18:48:45 +0300

    fix documentation

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -20,7 +20,7 @@ type CLI struct {
 		Login          string `help:"User login." short:"l"`
 		MasterPassword string `help:"User masterpassword." short:"p"`
 		SignIn         bool   `help:"Sign in now, if present user gets signed in instantly. Otherwise manual login is required (see login command)." short:"s"`
-	} `cmd:"" help:"Registration with auntication needs flag -s (empty flag means true)."`
+	} `cmd:"" help:"Registration with authentication needs flag -s (empty flag means true)."`
 
 	Login struct {
 		Login    string `help:"User login." short:"l"`
@@ -29,61 +29,59 @@ type CLI struct {
 
 	// ------------ password ------------
 	Passwords struct {
-	} `cmd:"" help:"List of passwords."`
+	} `cmd:"" help:"Shows list of passwords."`
 
 	AddPassword struct {
 		Password    string `help:"User password." short:"p"`
 		Title       string `help:"Password uniqe title." short:"t"`
 		Description string `help:"Password description." short:"d"`
-	} `cmd:"" help:"Password to add."`
+	} `cmd:"" help:"Adds password."`
 
 	GetPassword struct {
 		Title string `help:"Password uniqe title." short:"t"`
-	} `cmd:"" help:"Password to get by uniqe title."`
+	} `cmd:"" help:"Retrieves password by unique title."`
 
 	// UpdatePassword меняет поля (pwd, description) по title пароля
 	// если в json не передать поле, то оно не измениться
 	// если передать пустую строку "" - то поле станет пустым
-	// TODO а как поменять сам title????/
-	UpdatePassword struct { // HELP TODO CHECK
-		// ID          int    `help:"Password id, by id the password is deleted." short:"i"` // TODO не нужен?
-		Title       string `help:"Password uniqe title." short:"t"`
+	UpdatePassword struct {
+		Title       string `help:"Password unique title." short:"t"`
 		Password    string `help:"User password." short:"p"`
 		Description string `help:"Description." short:"d"`
-	} `cmd:"" help:"Password to update by uniqe title."`
+	} `cmd:"" help:"Updates password by unique title."`
 
 	DeletePassword struct {
 		Title string `help:"Password title." short:"t"`
-	} `cmd:"" help:"Password to delete by title."`
+	} `cmd:"" help:"Deletes password by unique title."`
 
 	// ------------ file ------------
 	AddFile struct {
 		FileName    string `help:"File path." short:"p"`
-		Title       string `help:"File uniqe title." short:"t"`
+		Title       string `help:"File unique title." short:"t"`
 		Description string `help:"Description." short:"d"`
-	} `cmd:"" help:"File to add."`
+	} `cmd:"" help:"Adds file."`
 
 	GetFile struct {
 		Title string `help:"File title." short:"t"`
-	} `cmd:"" help:"File to get by uniqe title."`
+	} `cmd:"" help:"Retrieves file by unique title."`
 	// TODO как написать, что сохранить под новым именем можно через >
 
 	Files struct {
-	} `cmd:"" help:"List of files added."`
+	} `cmd:"" help:"Shows list of added files."`
 
 	UpdateFile struct {
-		Title       string `help:"File uniqe title." short:"t"`
+		Title       string `help:"File unique title." short:"t"`
 		FileName    string `help:"File path." short:"p"`
 		Description string `help:"Description." short:"d"`
-	} `cmd:"" help:"File to update."`
+	} `cmd:"" help:"Updates file by unique title."`
 
 	DeleteFile struct {
-		Title string `help:"File uniqe title." short:"t"`
-	} `cmd:"" help:"File to delete by uniqe title."`
+		Title string `help:"File unique title." short:"t"`
+	} `cmd:"" help:"Delete file by unique title."`
 
 	// ------------ card ------------
 	Cards struct {
-	} `cmd:"" help:"List of cards."`
+	} `cmd:"" help:"Shows list of added cards."`
 
 	AddCard struct {
 		Num         string `help:"Card number, lehgth 16." short:"n"`
@@ -91,11 +89,11 @@ type CLI struct {
 		Cvc         string `help:"Card cvc, lehgth 3." short:"c"`
 		Title       string `help:"Card title, alphanumeric, min lehgth 4." short:"t"`
 		Description string `help:"Description." short:"d"`
-	} `cmd:"" help:"Card to add."`
+	} `cmd:"" help:"Adds card."`
 
 	GetCard struct {
 		Title string `help:"Card title." short:"t"`
-	} `cmd:"" help:"Card to get by uniqe title."`
+	} `cmd:"" help:"Retrieves card by unique title."`
 
 	UpdateCard struct {
 		Title       string `help:"Card title." short:"t"`
@@ -103,11 +101,11 @@ type CLI struct {
 		Expire      string `help:"Date of expire, lehgth 4." short:"e"`
 		Cvc         string `help:"Card cvc, lehgth 3." short:"c"`
 		Description string `help:"Description." short:"d"`
-	} `cmd:"" help:"Card to update."`
+	} `cmd:"" help:"Updates card by unique title."`
 
 	DeleteCard struct {
 		Title string `help:"Card title." short:"t"`
-	} `cmd:"" help:"Card to delete by uniqe title."`
+	} `cmd:"" help:"Deletes card by unique title."`
 }
 
 const service = "gokeeper"
